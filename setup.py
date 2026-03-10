07aad9f2909c2348551f9df1c01cd189f12409ce
jingzhi.xue <jingzhi.xue@mthreads.com>
2026-03-10 16:25:30 +0800

    bump to 2.2.11

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ finally:
 
 
 setup(name=_package_name,
-      version='2.2.10',
+      version='2.2.11',
       description='Python Bindings for the Moore Threads GPU Management Library',
       long_description=long_description,
       long_description_content_type='text/markdown',
